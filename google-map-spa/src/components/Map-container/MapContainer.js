c9ab587fcfea75090b8c7715ce385f0d7f000c6f
kudindmitriy <kudin.dima@gmail.com>
2021-01-19 01:31:57 +0200

    Added Current component.

--- google-map-spa/src/components/Map-container/MapContainer.js
+++ google-map-spa/src/components/Map-container/MapContainer.js
@@ -1,34 +1,70 @@
 import React, {Component} from 'react';
-import {Map, GoogleApiWrapper} from "google-maps-react";
-
-import {
-    WrapperMap
-} from './style'
+import { GoogleApiWrapper, InfoWindow, Marker} from "google-maps-react";
+import CurrentLocation from "../CurrentLocation";
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+};
 
 
 export class MapContainer extends Component{
 
-    render() {
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+    }
 
-        const { google } = this.props
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
+
+    canterMoved = () => {
+
+    }
+
+
+render() {
+        const { google  } = this.props
 
         return(
-                <WrapperMap>
-                    <Map
+                    <CurrentLocation
+                        centerAroundCurrentLocation
                         google={google}
-                        zoom={14}
-                        initialCenter={
-                            {
-                                lat: 47.8563228,
-                                lng: 35.1051424
-                            }
-                        }
-                    />
-                </WrapperMap>
+                        style={mapStyles}
+                        onDblclick={() => console.log('Is clicked') }
+
+                    >
+                        <Marker
+                            onClick={this.onMarkerClick}
+                            name={'Familiar District'}
+                        />
+                        <InfoWindow
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}
+                            onClose={this.onClose}
+                        >
+                            <div>
+                                <h4>{this.state.selectedPlace.name}</h4>
+                            </div>
+                        </InfoWindow>
+                    </CurrentLocation>
             )
     }
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'Any_Key_Pass'
-})(MapContainer)
+     apiKey: "https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyDj0MDTfzDhcUevW7Vpd7YA_fGn1htSdFM"
+    })(MapContainer)
